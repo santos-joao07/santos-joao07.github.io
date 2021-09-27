b197add5b3dca366019d1c6e1722f7d1dcd882ee
santos-joao07 <santos.joao@unifesspa.edu.br>
2021-09-27 19:30:02 -0300

    atividade 02

---
+++ AULA-04/ATIVIDADE-03/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (request, response) => {
+    console.log(request.body)
+    console.log(request.query)
+    return response.send(`<h1>Parabéns! Usuário(a): ${request.body.name}, incluído(a) com sucesso!</h1>`)
+})
+
+app.listen(3003, () => {
+    console.log('O servidor está rodando na porta: 3333');
+})
